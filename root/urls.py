8e949288637513246b57a7251ccf2be78ead6447
furukawa2525 <ksw2070339@stu.o-hara.ac.jp>
2021-11-23 23:58:09 +0900

    rooturl修正

--- root/urls.py
+++ root/urls.py
@@ -5,6 +5,5 @@ app_name = 'root'
 urlpatterns = [
     path('', views.TopView.as_view(), name="top"),
     path('map_list/', views.MapListView.as_view(), name="map_list"),
-    path('map_list/', views.MapListView.as_view(), name="map_create"),
     path('map_create/', views.MapCreateView.as_view(), name="map_create"),
 ]
