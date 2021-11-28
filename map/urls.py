8e8bf1dfa6d4c97123b8f2a1904040028df1ae39
furukawa2525 <ksw2070339@stu.o-hara.ac.jp>
2021-11-29 03:42:14 +0900

    検索機能実装

--- map/urls.py
+++ map/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('', views.TopView.as_view(), name="top"),
     path('map_list/', views.MapListView.as_view(), name="map_list"),
     path('user_list/', views.MapCreateView.as_view(), name="user_list"),
-    path('ajax_post_search/', views.MapCreateView.as_view(), name="ajax_post_search"),
+    path('ajax_post_search/', views.ajax_post_search, name="ajax_post_search"),
 ]
--- map/views.py
+++ map/views.py
@@ -16,17 +16,16 @@ class MapListView(generic.ListView):
 class MapCreateView(generic.ListView):
     model = User
     
-    def ajax_post_search(request):
-        keyword = request.GET.get('name')
-
-        # 検索キーワードがあればそれで絞り込み、なければ全ての記事
-        # JSONシリアライズするには、Querysetをリストにする必要あり
-        if keyword:
-            name_list = [row.address for row in User.objects.filter(name__icontains=keyword)]  # タイトルにキーワードを含む。大文字小文字の区別なし
-        else:
-            name_list = [row.address for row in User.objects.order_by('facility').all()]
-
-        d = {
-            'name_list': name_list,
-        }
-        return JsonResponse(d)
+def ajax_post_search(request):
+    keyword = request.GET.get('user')
+    # 検索キーワードがあればそれで絞り込み、なければ全ての記事
+    # JSONシリアライズするには、Querysetをリストにする必要あり
+    if keyword:
+        name_list = [[column.name, column.address] for column in User.objects.filter(name__icontains=keyword)]  # nameにキーワードを含む。大文字小文字の区別なし
+    else:
+        # name_list = [column.name for column in User.objects.all()]
+        name_list = ["一致する人物が見つかりませんでした。"]
+    d = {
+        'name_list': name_list,
+    }
+    return JsonResponse(d)
