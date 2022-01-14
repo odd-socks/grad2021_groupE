9b63837083dd8f965b31924b0c3b823c753e8c6a
KamimuraAkito <akito0702kamimura@gmail.com>
2022-01-14 10:54:51 +0900

    bootstarp反映

--- map/urls.py
+++ map/urls.py
@@ -12,11 +12,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+#from django.contrib import path
+from django.urls import path
+from .import views
 
+app_name = 'map'
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('map.urls')),
-    # path('accounts/', include('allauth.urls')),
+    path('index', views.IndexView.as_view(), name="index"),
 ]
