133f2b35faba4729cdcefd0bd3d7335f4c9e0075
KamimuraAkito <akito0702kamimura@gmail.com>
2022-01-18 16:35:16 +0900

    モデル作成途中

--- map/models.py
+++ map/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+# class Facility(models.Model):
+#     facility_name = models.CharField(max_length=200)#施設名
+#     password = models.CharField(max_length=200)#パスワード
+#     address = models.CharField(max_length=255)#住所
+
+# class Underwriter(models.Model):
+#     username = models.CharField(max_length=200)#氏名
+#     password = models.CharField(max_length=255)#パスワード
