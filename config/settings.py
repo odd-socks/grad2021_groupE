182e9da251e26ad34043fbd30ff5f9b5617a1ebf
KamimuraAkito <akito0702kamimura@gmail.com>
2022-01-18 14:18:55 +0900

    再初期設定

--- config/settings.py
+++ config/settings.py
@@ -36,12 +36,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'map.apps.MapConfig',
-    'accounts.apps.AccountsConfig',#ユーザー認証
+    #'accounts.apps.AccountsConfig',#ユーザー認証
 
 
-    'django.contrib.sites',#for djnago-allauth
-    'allauth',#for djnago-allauth
-    'allauth.account',#for djnago-allauth
+    #'django.contrib.sites',#for djnago-allauth
+    #'allauth',#for djnago-allauth
+    #'allauth.account',#for djnago-allauth
 ]
 
 MIDDLEWARE = [
@@ -59,7 +59,8 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [],
+        #os.path.join(BASE_DIR, 'templates')
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -128,9 +129,3 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-
-SITE_ID = 1 #追記20210116
-
-# 追加
-LOGIN_URL = 'map:login' # ログインのURLの設定
-LOGIN_REDIRECT_URL = 'map:index' #ログインが完了した後に遷移するURL
--- config/urls.py
+++ config/urls.py
@@ -19,17 +19,7 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('map.urls')),
-    path('accounts/', include('allauth.urls')),
-
-    path('accounts/login/', TemplateView.as_view(template_name = 'login.html'), name='login'),
-    path('accounts/logout/', TemplateView.as_view(template_name = 'logout.html'), name='logout'),
-    path('accounts/logout_done/', TemplateView.as_view(template_name = 'logout_done.html'), name='logout_done'),
-    path('accounts/signup/', TemplateView.as_view(template_name = 'signup.html'), name='signup'),
-    path('accounts/sign_up_done/', TemplateView.as_view(template_name = 'sign_up_done.html'), name='sign_up_done'),
-    path('accounts/my_page/', TemplateView.as_view(template_name = 'my_page.html'), name='my_page'),
-    path('accounts/my_pate/', TemplateView.as_view(template_name = 'my_pate.html'), name='my_pate'),
-    path('accounts/password_change/', TemplateView.as_view(template_name = 'password_change.html'), name='password_change'),
-    path('accounts/user_form/', TemplateView.as_view(template_name = 'user_form.html'), name='user_form'),
+    #path('accounts/', include('allauth.urls')),
 
 
 
